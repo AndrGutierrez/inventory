4e4623ad317b93b7541fc2263b627aeb781b1ec0
Andres Gutierrez <andresgutierrezdev@gmail.com>
2023-07-01 21:40:30 -0400

    adding user input, just a bit

--- main.cpp
+++ main.cpp
@@ -49,14 +49,69 @@ int calc_time(function<void()> func){
     return 0;
 }
 
+void clientFlow(){
+
+		MergeSort<Client> mergeSort;
+		Search searcher;
+		int accion;
+		string name;
+		
+		cout << "Seleccione Que Desea Hacer: " << endl;
+		printf("1. Listar\n2. Ordenar y listar\n3. Buscar \nIngrese el Numero de correspondiente a la accion: ");
+		cin >> accion;
+		DBConnection<Client> clientConnection= DBConnection<Client>();
+		switch(accion){
+			case 1:
+				clientConnection.selectAll("db/client.dat");
+				break;
+			case 2:
+				mergeSort.run();
+				remove("db/client_copy.dat");
+				break;
+			case 3:
+				cout << "Ingrese el nombre que quiere buscar: " << endl;
+				cin >> name;
+				mergeSort.run();
+				searcher.runIndexed(name);
+				remove("db/client_copy.dat");
+				break;
+			default:
+				cout << "Opcion Invalida" << endl;
+				break;
+		}
+
+}
+
+void productFlow(){
+
+}
+
+void getUserInput(){
+
+		int accion;
+		cout << "Seleccione Que Desea Hacer: " << endl;
+		printf("1. Operaciones con Productos\n2. Operaciones con clientes\n Ingrese el Numero de correspondiente a la accion: ");
+		cin >> accion;
+		switch(accion){
+			case 1:
+				clientFlow();
+				break;
+			case 2:
+				productFlow();
+				break;
+			default:
+				cout << "Opcion Invalida" << endl;
+				break;
+		}
+
+}
+
 int main(){
-	// DBConnection<Client> clientConnection= DBConnection<Client>();
+	DBConnection<Client> clientConnection= DBConnection<Client>();
 	// string data=clientConnection.selectAll("db/client.dat");
 	// cout<<data<<endl;
 	// Inventory inventory;
 	// inventory.run();
 	// calc_time(quicksort);
-	MergeSort<Client> mergeSort;
-	mergeSort.run();
 	return 0;
 }
